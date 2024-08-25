127cbee78d08cc894ab873bcf81e53eeb402f433
Lucas Azevedo <84247452+luczevz@users.noreply.github.com>
2024-08-25 02:32:22 -0300

    Fundamentos C# Start

---
+++ FundamentsCsharp.Console/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region Tipos e Variáveis
+
+//var nome = Console.ReadLine();
+
+//Console.WriteLine(nome);
+
+//double valorDouble = 3.14;
+//float valorFloat = 3.14F;
+//decimal valorDecimal = 3.14M;
+//int valorInteiro = 100;
+//bool valorBoolean = true;
+//char valorChar = 'A';
+//string valorString = "Lucas Azevedo";
+
+//const double PI = 3.1416;
+
+//Console.WriteLine($" Double: {valorDouble},  Float:{valorFloat},  Decimal: {valorDecimal}," +
+//    $" Char: {valorChar}, Bool: {valorBoolean}, Int: {valorInteiro}, String: {valorString}");
+
+//// 
+
+
+
+#endregion
+
+#region Operadores Aritméticos
+
+/*-Adição +
+ 
+   -Subtração -
+    -Multiplicação *
+    -Divisão /
+    -Módulo %
+
+int a = 15;
+int b = 20;
+
+int soma = a + b;
+int subtracao = a - b;
+int multiplicacao = a * b;
+int divisao = b / a;
+int modulo = a % b;
+
+Console.WriteLine($"Soma:{soma} ,Subtração: {subtracao} , Multiplicação: " +
+    $"{multiplicacao} , Divisão: {divisao} , Modulo: {modulo}, ");*/
+
+#endregion 
+
+#region Operadores de Comparação
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endregion
